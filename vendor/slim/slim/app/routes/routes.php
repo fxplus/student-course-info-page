2b4e4239fe21aed17be52d60a16460e65db70f6e
Aaron Marr <aaronmarruk@gmail.com>
2014-08-04 11:45:56 +0100

    Rename app. Is now called app and not router

--- vendor/slim/slim/app/routes/routes.php
+++ vendor/slim/slim/app/routes/routes.php
@@ -1,23 +1,23 @@
 <?php
 
-$router->get('/', function () {
+$app->get('/', function () {
     echo "Hi there";
 });
 
-$router->get('/hello/:name', function ($name) {
+$app->get('/hello/:name', function ($name) {
     echo "Hello, $name";
 });
 
-$router->get('/ba-film', function () {
+$app->get('/ba-film', function () {
     echo "You have been redirected to a SCIP";
 });
 
-$router->get('/ba-music', function () {
+$app->get('/ba-music', function () {
     echo "You have been redirected to Learning Space";
 });
 
-$router->get('/am144296', function () use ($router) {    
-    $router->redirect('http://localhost:8888/scip/');
+$app->get('/am144296', function () use ($app) {    
+    $app->redirect('http://localhost:8888/scip/');
 });
 
 ?>
--- vendor/slim/slim/index.php
+++ vendor/slim/slim/index.php
@@ -5,12 +5,12 @@ require 'Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
 
 // Instantiate a new slim app ("In this case called router")
-$router = new \Slim\Slim();
+$app = new \Slim\Slim();
 
 // Load the routes
 require 'app/routes/routes.php';
 
 // Run the app
 // This should always come last.
-$router->run();
+$app->run();
 ?>
