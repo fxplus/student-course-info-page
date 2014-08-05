8beb9254dbcc5cdc5446244f0766a364ecd7c193
Aaron Marr <aaronmarruk@gmail.com>
2014-08-05 13:55:18 +0100

    Fake API call, pass json to angular views

--- vendor/slim/slim/routes/routes.php
+++ vendor/slim/slim/routes/routes.php
@@ -1,25 +1,30 @@
 <?php
 
-$app->get('/', function () {
-    echo "Hi there";
-});
+//$app->get('/', function () {
+//    echo "Hi there";
+//});
+
+$app->get('/', function () use ($app) {
+    $app->render('home.php');
+})->name('home');
 
 $app->get('/hello/:name', function ($name) {
     echo "Hello, $name";
 });
 
-$app->get('/ba-film', function () {
-    echo "You have been redirected to a SCIP";
-});
-
-$app->get('/ba-music', function () {
-    echo "You have been redirected to Learning Space";
-});
-
 $app->get('/am144296', function () use ($app) {  
-	// It appears you have to configure the view dir on each call so:
-	//$app->config('templates.path', realpath(__DIR__ . '/..') . '/views/');   
-    $app->render('index.php');
+	
+	// Fake the API call
+    $url = 'https://gist.githubusercontent.com/aaronmarruk/b116956eaf7254532c99/raw/be640546d647f0ae9b7041c23901e21e0fdbb008/json-response.json';
+    $curlSession = curl_init();
+    curl_setopt($curlSession, CURLOPT_URL, $url);
+    curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
+    curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
+    $jsonData = curl_exec($curlSession);
+    curl_close($curlSession);
+   
+    $app->render('index.php', array('json' => $jsonData));
+
 });
 
 ?>
--- vendor/slim/slim/templates/index.php
+++ vendor/slim/slim/templates/index.php
@@ -45,40 +45,14 @@
 
   // Set up the information page controller
   app.controller('InformationPageController', function($scope) {
-    // Example response from SharePoint API
-      $scope.response = {
+    
+    // Example response from SharePoint API, 
+    // passed in from the router on server (for caching reasons).
+    <?php  echo 'var response = '.$this->data['json']; ?>;
 
-        "contacts": [
-          { 
-            name: "Russel Clark",
-            email: "russel@falmouth.ac.uk",
-            telephone: "01326 789 876"
-          },
-          { 
-            name: "Al Parker",
-            email: "alparker@falmouth.ac.uk",
-            telephone: "01326 765 678"
-          },
-          { 
-            name: "John Parker Rees",
-            email: "jpr@falmouth.ac.uk",
-            telephone: "01326 244 355"
-          } 
-        ],
-
-        "files": [
-          { 
-            title: "SharePoint 2013 WCM Advanced Cookbook",
-            link: "http://it-ebooks.info/book/3167/",
-          },
-          { 
-            title: "Microsoft SharePoint 2013 App Development",
-            link: "http://it-ebooks.info/book/3079/",
-          } 
-        ]
-
-      }
-    });
+    // Set scope response to API response.
+    $scope.response = response;
+  });
 
   </script>
 
