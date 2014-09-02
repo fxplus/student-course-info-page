9e558dca6a6076ffb314ea694a9ea3f3932b3bbe
Aaron Marr <aaronmarruk@gmail.com>
2014-09-02 14:22:54 +0100

    Left an echo statement in the controller

--- routers/courselist.router.php
+++ routers/courselist.router.php
@@ -8,8 +8,6 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
 
-echo "Porridge";
-
 require_once('moodlequery/config.php');
 require_once('moodlequery/class.moodlequery.php');
 require_once('moodlequery/class.aspireapi.php'); 
